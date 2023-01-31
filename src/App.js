acb4adb1d5bcca7bbd305bf3b8161baa3a878c0a
jainakshay1 <37770903+jainakshay1@users.noreply.github.com>
2023-01-31 16:26:01 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Form from './Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        This Portal is only available for RG creation You will be able to provison remaining resources soon
+      </h1>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
